efe0f1403e166c62b794820690990a814e08acbe
Amber Lin <amberlin618@gmail.com>
2025-09-08 09:17:30 -0700

    set up animator blend tree

--- Assets/Scripts/Player/SpriteAnimator.cs
+++ Assets/Scripts/Player/SpriteAnimator.cs
@@ -8,21 +8,27 @@ public class PlayerSpriteFlip : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
     public Animator animator;
-
-
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-
+    private bool IsGrounded()
+    {
+        Collider2D col = GetComponent<Collider2D>();
+        Vector2 origin = new Vector2(col.bounds.center.x, col.bounds.min.y);
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.05f, LayerMask.GetMask("Ground"));
+        return hit.collider != null;
+    }
     private void Update()
     {
         float horizontalSpeed = rb.linearVelocity.x;
 
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.linearVelocity.x));
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
 
-        if(Mathf.Abs(horizontalSpeed) > 0.1f) animator.SetBool("running", true);
-        else if(Mathf.Abs(horizontalSpeed) < 0.1f) animator.SetBool("running", false);
+        animator.SetBool("running", Mathf.Abs(horizontalSpeed) > 0.1f);
 
         if (horizontalSpeed > 0.1f)
         {
@@ -30,7 +36,11 @@ public class PlayerSpriteFlip : MonoBehaviour
         }
         else if (horizontalSpeed < -0.1f)
         {
-            spriteRenderer.flipX = true; 
+            spriteRenderer.flipX = true;
         }
+
+        // animator.SetBool("jumping", rb.linearVelocity.y > 0.1f || !IsGrounded());
+
+        
     }
 }
