5a4c08067c42dfd69ce1a70192429cfe7baf1264
Marcel Relaford <iammarcel08@gmail.com>
2026-08-19 21:31:32 -0700

    add bob to item to be picked up

--- Assets/Scripts/Interactable/ItemPickup.cs
+++ Assets/Scripts/Interactable/ItemPickup.cs
@@ -4,6 +4,14 @@ public class ItemPickup : Interactible
 {
     [SerializeField] private Item item;
     [SerializeField] private int quantity = 1;
+    private readonly float BobSpeed = 2.3f;
+    private readonly float BobHeight = .1f;
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
 
     public override void Interact()
     {
@@ -12,5 +20,12 @@ public class ItemPickup : Interactible
         {
             Destroy(gameObject);
         }
+    }
+
+    void Update()
+    {
+        float yOffset = Mathf.Sin(Time.time * BobSpeed) * BobHeight;
+
+        transform.position = startPosition + new Vector3(0, yOffset, 0);
     }
 }
